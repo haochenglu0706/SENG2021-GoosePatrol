f9e0cf2755ae6aff2c58611cef1551d30932876a
Haocheng <luhaocheng05@gmail.com>
2026-03-14 23:38:55 +1100

    added init try catch logic to getPost in despatchAdvice

--- src/handlers/despatchAdvice.js
+++ src/handlers/despatchAdvice.js
@@ -8,3 +8,12 @@ const {
 } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
+const getPost = async (event) => {
+    const response = { statusCode: 200 };
+
+    try {
+        
+    } catch (e) {
+
+    }
+};
