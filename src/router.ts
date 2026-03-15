4a01dac093c860a14d89e8cbdf3d5ac5eab5013c
Jingwei Chen <ianchen7007@gmail.com>
2026-03-16 02:15:27 +1100

    implemented delete and put method for despatch

--- src/router.ts
+++ src/router.ts
@@ -46,14 +46,16 @@ export async function route(event: any) {
     if (method === "DELETE") {
       return despatch.deleteDespatchAdvice(event, documentId, sessionId);
     }
-  // GET /despatch-advices/{despatchId}
-  const despatchAdviceMatch = path.match(/^\/despatch-advices\/([^/]+)$/);
-  if (method === "GET" && despatchAdviceMatch) {
-    event.pathParameters = {
-      ...event.pathParameters,
-      despatchId: despatchAdviceMatch[1],
-    };
-    return despatch.getDespatchAdvice(event);
+
+    // GET /despatch-advices/{despatchId}
+    const despatchAdviceMatch = path.match(/^\/despatch-advices\/([^/]+)$/);
+    if (method === "GET" && despatchAdviceMatch) {
+      event.pathParameters = {
+        ...event.pathParameters,
+        despatchId: despatchAdviceMatch[1],
+      };
+      return despatch.getDespatchAdvice(event);
+    }
   }
 
   // RECEIPT ADVICE ROUTES
